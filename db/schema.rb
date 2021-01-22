ddce91eb521c1e50ec8749d63e0f7c32eb655b73
Ceegan Laubscher <ceeganlaub@gmail.com>
2021-01-22 20:38:03 +0200

    Blog Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_193834) do
+ActiveRecord::Schema.define(version: 2021_01_22_183251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "guides", force: :cascade do |t|
     t.string "title"
